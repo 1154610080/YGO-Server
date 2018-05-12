0e7acce90d09bc7186f08132992dac6834fb70c9
Egan <1154610080@qq.com>
2018-05-12 10:15:49 +0800

    完成Gson封装类

---
+++ LobbyServer/src/main/java/com/ygo/util/GsonWrapper.java
@@ -0,0 +1,45 @@
+package com.ygo.util;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Gson封装类
+ * 统一设置字段忽略策略
+ * 
+ * @author Egan
+ * @date 2018/5/12 10:15
+ **/
+
+public class GsonWrapper{
+    
+    private GsonBuilder builder;
+    
+    public GsonWrapper(){
+        builder = new GsonBuilder();
+        
+        setExclusionStrategies();
+        
+    }
+    
+    private void setExclusionStrategies(){
+        builder.serializeNulls().setExclusionStrategies(new ExclusionStrategy() {
+            @Override
+            public boolean shouldSkipField(FieldAttributes fieldAttributes) {
+                String name = fieldAttributes.getName();
+                return name.contains("password") || name.contains("ip") || name.contains("port");
+            }
+
+            @Override
+            public boolean shouldSkipClass(Class<?> aClass) {
+                return false;
+            }
+        });
+    }
+    
+    public Gson create(){
+        return builder.create();
+    }
+}
