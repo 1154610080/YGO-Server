e7440a0cda62509cc97df257646b6290b4a105d4
Egan <1154610080@qq.com>
2018-05-12 09:48:37 +0800

    完成游戏大厅-决斗服务器类

---
+++ LobbyServer/src/main/java/com/ygo/server/LDServer.java
@@ -0,0 +1,45 @@
+package com.ygo.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * 游戏大厅-决斗服务器
+ * 游戏大厅服务器和决斗服务器之间的socket服务器
+ *
+ * @author Egan
+ * @date 2018/5/12 9:34
+ **/
+public class LDServer {
+
+    private int port = 0000;
+
+    public void start() throws InterruptedException {
+        LDServerHandler handler = new LDServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(handler);
+                        }
+                    });
+            ChannelFuture f = bootstrap.bind().sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            group.shutdownGracefully();
+        }
+    }
+
+}
