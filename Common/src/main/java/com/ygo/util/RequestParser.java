5aec693744422505ab73ffcacc28cf4b4beed70f
Egan <1154610080@qq.com>
2018-05-14 22:27:51 +0800

    删除Get请求解析方法

--- Common/src/main/java/com/ygo/util/RequestParser.java
+++ Common/src/main/java/com/ygo/util/RequestParser.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Http请求解析类
+ * Http POST请求解析类
  *
  * @author Egan
  * @date 2018/5/7 22:53
@@ -19,7 +19,7 @@ import java.util.Map;
 public class RequestParser {
 
     /**
-     * 解析请求
+     * 解析POST请求
      *
      * @date 2018/5/7 23:22
      * @return java.util.Map<java.lang.String,java.lang.String>
@@ -29,28 +29,16 @@ public class RequestParser {
 
         Map<String, String> paramMap = new HashMap<>();
 
-        if(HttpMethod.GET == method){
-            //Get请求
-            QueryStringDecoder decoder = new QueryStringDecoder(req.uri());
+        HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(req);
+        decoder.offer((HttpContent) req);
 
-            decoder.parameters().forEach((key, value) -> {
-                //entry是一个list，只取第一个元素
-                paramMap.put(key, value.get(0));
-            });
-        }else if(HttpMethod.POST == method){
-            //Post请求
-            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(req);
-            decoder.offer((HttpContent) req);
-
-            List<InterfaceHttpData> paramList = decoder.getBodyHttpDatas();
+        List<InterfaceHttpData> paramList = decoder.getBodyHttpDatas();
             
-            for(InterfaceHttpData param : paramList){
-                Attribute data = (Attribute)param;
-                paramMap.put(data.getName(), data.getValue());
-            }
-        }else{
-            System.err.println("错误：不支持其他方法！");
+        for(InterfaceHttpData param : paramList){
+            Attribute data = (Attribute)param;
+            paramMap.put(data.getName(), data.getValue());
         }
+
 
         return paramMap;
     }
