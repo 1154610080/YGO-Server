be287dfdad01452eec78c10713396609897a93e7
Egan <1154610080@qq.com>
2018-05-14 21:23:55 +0800

    tiny optimization

--- Common/src/main/java/com/ygo/util/ResponseStatus.java
+++ Common/src/main/java/com/ygo/util/ResponseStatus.java
@@ -5,14 +5,14 @@ import org.apache.http.HttpStatus;
 import java.util.HashMap;
 
 /**
- * ??????
+ * Http响应状态类
  *
  * @author Egan
  * @date 2018/5/12 9:15
  **/
 public class ResponseStatus extends HashMap<String, Object>{
 
-    ResponseStatus(int code, String msg){
+    public ResponseStatus(int code, String msg){
         super.put("code", code);
         super.put("msg", msg);
     }
